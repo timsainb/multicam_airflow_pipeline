0aaa2eab29efb9eb1641c101c9173e22807472b2
Jonah Pearl <j4jonahp@gmail.com>
2024-09-09 21:04:04 -0400

    debug

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ def read_requirements():
     with open('requirements.txt') as f:
         return f.read().splitlines()
 
+print("in setup.py")
+
 setup(
     name='multicamera-airflow-pipeline',
     version='0.1.0',
