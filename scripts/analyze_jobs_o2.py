2c7e46dab86c2f77a74a721e1e720e324833687d
Jonah Pearl <jonahpearl@g.harvard.edu>
2024-10-16 10:22:19 -0400

    report jobids for ease of analysis

--- scripts/analyze_jobs_o2.py
+++ scripts/analyze_jobs_o2.py
@@ -25,6 +25,7 @@ def main(airflow_logs_file):
     for task in tasks_to_analyze:
         time_fractions = []
         mem_fractions = []
+        jobids = []
         for run_name, run_info in logs.items():
             # Skip stuff we dont want
             if (
@@ -45,16 +46,18 @@ def main(airflow_logs_file):
             # Save the info
             time_fractions.append(jobinfo[jobid]["time_fraction"])
             mem_fractions.append(jobinfo[jobid]["mem_fraction"])
+            jobids.append(jobid)
 
         # Report the results
         print(f"Task: {task}")
         print(f"\tAnalyzed {len(time_fractions)} jobs")
-        print(f"\tMedian time fraction: {np.median(time_fractions):.3f}")
+        print(f"\tJob ids: {jobids}")
         print(f"\tMinimum time fraction: {np.min(time_fractions):.3f}")
+        print(f"\tMedian time fraction: {np.median(time_fractions):.3f}")
         print(f"\tMaximum time fraction: {np.max(time_fractions):.3f}")
-
-        print(f"\tMedian mem fraction: {np.median(mem_fractions):.3f}")
+        print()
         print(f"\tMinimum mem fraction: {np.min(mem_fractions):.3f}")
+        print(f"\tMedian mem fraction: {np.median(mem_fractions):.3f}")
         print(f"\tMaximum mem fraction: {np.max(mem_fractions):.3f}")
 
     # TODO: Plot the results
